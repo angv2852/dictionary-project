5097e2cedd6e335c7e9b048fc734349e64456247
Ang <angv2852@gmail.com>
2022-01-28 12:20:07 -0700

    Added Axios & dictionary API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   const [input, setInput] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`Searching for the definition of ${input}`);
+
+    //documentation: https://api.dictionaryapi.dev/
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${input}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleInput(event) {
